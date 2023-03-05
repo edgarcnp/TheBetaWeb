e859dac0be20b962deff0d45c17b1a396033c8be
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-06 00:51:23 +0700

    urls stuff

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def room(request, room):
+    return HttpResponse(f"You're looking at room {room}.")
