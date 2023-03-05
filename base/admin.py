b547e59b01b3d26c0ffc9a9306a023ce562dc1bc
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-06 04:02:35 +0700

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
