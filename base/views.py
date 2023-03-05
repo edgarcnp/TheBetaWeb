9fdd637fba6cb73b9a307a7ae62227809b8b2382
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-06 01:36:21 +0700

    templates stuff

--- base/views.py
+++ base/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "home.html")
 
 
 def room(request, room):
-    return HttpResponse(f"You're looking at room {room}.")
+    return render(request, "room.html", {"room_name": room})
