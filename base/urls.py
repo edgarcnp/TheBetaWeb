02f3a07fe4c111e244e85ecabadf346b7bd39823
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-06 10:49:37 +0700

    create-room, form Model

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/<str:room>", views.room, name="room"),
+    path("create-room/", views.create_room, name="create-room"),
+    path("update-room/<str:room_id>", views.update_room, name="update-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import RoomForm
 from .models import Room
 
 # Create your views here.
@@ -15,3 +16,28 @@ def room(request, room_id):
     rooms = Room.objects.get(id=room_id)
     respond = {"rooms": rooms}
     return render(request, "base/room.html", respond)
+
+
+def create_room(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    respond = {"form": form}
+    return render(request, "base/room_form.html", respond)
+
+
+def update_room(request, room_id):
+    room_update = Room.objects.get(id=room_id)
+    form = RoomForm(instance=room_update)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room_update)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    respond = {"form": form}
+    return render(request, "base/room_form.html", respond)
