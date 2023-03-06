99e9697aede307d529d018f3b20da975ede41fce
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-06 11:57:39 +0700

    migrate (ordering topic)

--- base/models.py
+++ base/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
